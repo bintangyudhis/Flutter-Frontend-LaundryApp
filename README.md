f1163d305834a13a0acd8cdfed4af3f114daeacb
bintangyudhis <bintangyudhis333@gmail.com>
2026-01-27 12:31:41 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# frontend_dilaundry
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.light(
+          primary: Colors.green,
+          secondary: Colors.greenAccent[400]!,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: const WidgetStatePropertyAll(Colors.green),
+            shape: WidgetStatePropertyAll(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+            ),
+            padding: const WidgetStatePropertyAll(
+              EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+            ),
+            textStyle: WidgetStatePropertyAll(TextStyle(fontSize: 15)),
+          ),
+        ),
+      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
