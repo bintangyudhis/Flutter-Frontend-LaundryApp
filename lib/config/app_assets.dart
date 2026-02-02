9f22e19df6b23fdfffa0635da0fd89b9e279065e
bintangyudhis <bintangyudhis333@gmail.com>
2026-02-02 17:15:37 +0700

    pembuatan asset

---
+++ lib/config/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String bgAuth = 'assets/bg_auth.jpg';
+  static const String placeHolderLaundry = 'assets/placeholder_laundry.jpg';
+  static const String profile = 'assets/profile.jpg';
+  static const String emptyBg = 'assets/empty_background.jpg';
+  static const String wa = 'assets/wa.png';
+
+}
